14c14e2b28a2a0dae9f6f1b49752024d6a079745
smalhi5 <simmalhi123@gmail.com>
2017-07-28 13:32:53 -0600

    Adding the Eistmote-SDK and BrowsingBeacon

---
+++ BrowsingBeacon/app/src/main/java/com/example/simran/browsingbeacon/MyAppication.java
@@ -0,0 +1,13 @@
+package com.example.simran.browsingbeacon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyAppication extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_appication);
+    }
+}
